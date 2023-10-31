7d2322903683d9ed3c56f877f8bd23bdd5f07f20
MdShijanAli <freelancershijan@gmail.com>
2023-10-31 23:15:27 +0600

    hover checkbox effect done

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,15 @@ import { useState } from "react";
 
 const App = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [checkedImages, setCheckedImages] = useState(new Array(11).fill(false)); // Use the total number of images
 
-  const images = [
-    // Replace with your image source URLs
-    img1,
-    img2,
-    img3,
-    img4,
-    img5,
-    img6,
-    img7,
-    img8,
-    img9,
-    img10,
-    img11,
-  ];
+  const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11];
+
+  const handleImageClick = (index) => {
+    const updatedCheckedImages = [...checkedImages];
+    updatedCheckedImages[index] = !updatedCheckedImages[index];
+    setCheckedImages(updatedCheckedImages);
+  };
 
   return (
     <div>
@@ -39,35 +33,65 @@ const App = () => {
             <img className="border rounded-md" src={images[0]} alt="" />
           </div>
           <div className="col-span-3 grid grid-cols-3 gap-5">
-  {images.slice(1, 7).map((src, index) => (
-    <div key={index}>
-      <div className="relative group" onMouseEnter={() => setHoveredIndex(index + 1)} onMouseLeave={() => setHoveredIndex(null)}>
-        <img className={`border rounded-md transition-opacity ${ index + 1 === hoveredIndex ? 'opacity-50' : 'opacity-100' }`} src={src} alt=""/>
-        {index + 1 === hoveredIndex && (
-          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-90">
-            <input type="checkbox" className="text-green-500 w-6 h-6" />
+            {images.slice(1, 7).map((src, index) => (
+              <div key={index}>
+                <div
+                  className="relative group"
+                  onMouseEnter={() => setHoveredIndex(index + 1)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                  onClick={() => handleImageClick(index + 1)}
+                >
+                  <img
+                    className={`border rounded-md transition-opacity ${
+                      index + 1 === hoveredIndex ? 'opacity-50' : 'opacity-100'
+                    }`}
+                    src={src}
+                    alt=""
+                  />
+                  {index + 1 === hoveredIndex && (
+                    <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-90">
+                      <input
+                        type="checkbox"
+                        className="text-green-500 w-6 h-6"
+                        checked={checkedImages[index + 1]}
+                        onChange={() => handleImageClick(index + 1)}
+                      />
+                    </div>
+                  )}
+                </div>
+              </div>
+            ))}
           </div>
-        )}
-      </div>
-    </div>
-  ))}
-</div>
-
         </div>
         <div className="grid grid-cols-5 gap-5 mt-5">
-          {images.slice(7, 11).map((src, index) => (
-            <div key={index}>
-              <div className="relative group" onMouseEnter={() => setHoveredIndex(index + 7)} onMouseLeave={() => setHoveredIndex(null)}>
-                <img className={`border rounded-md transition-opacity ${ index + 7 === hoveredIndex ? 'opacity-50' : 'opacity-100' }`} src={src} alt=""/>
-                {index + 7 === hoveredIndex && (
-                  <div className="absolute top-10 left-10 w-full h-full  bg-gray-800 bg-opacity-90">
-                    <input type="checkbox" className="text-green-500 w-6 h-6" />
-                  </div>
-                )}
+        {images.slice(7, 11).map((src, index) => (
+              <div key={index}>
+                <div
+                  className="relative group"
+                  onMouseEnter={() => setHoveredIndex(index + 7)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                  onClick={() => handleImageClick(index + 7)}
+                >
+                  <img
+                    className={`border rounded-md transition-opacity ${
+                      index + 7 === hoveredIndex ? 'opacity-50' : 'opacity-100'
+                    }`}
+                    src={src}
+                    alt=""
+                  />
+                  {index + 7 === hoveredIndex && (
+                    <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-90">
+                      <input
+                        type="checkbox"
+                        className="text-green-500 w-6 h-6"
+                        checked={checkedImages[index + 7]}
+                        onChange={() => handleImageClick(index + 7)}
+                      />
+                    </div>
+                  )}
+                </div>
               </div>
-              
-            </div>
-          ))}
+            ))}
           <div className="flex items-center justify-center w-full h-full">
             <label
               htmlFor="dropzone-file"
@@ -86,6 +110,6 @@ const App = () => {
       </div>
     </div>
   );
-}  
+};
 
 export default App;
